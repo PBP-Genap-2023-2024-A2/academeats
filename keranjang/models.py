1833c514342e6f33aaf920ef2ed91e18738f5ea7
Khair <khairsmp1@gmail.com>
2024-03-29 23:14:04 +0700

    Adding keranjang model

--- keranjang/models.py
+++ keranjang/models.py
@@ -4,4 +4,4 @@ from user_profile.models import User
 
 # Model Keranjang
 class Keranjang(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
