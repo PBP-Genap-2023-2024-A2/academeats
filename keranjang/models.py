1c2ec40d774d3f6116b4329c6df566f5452b1d4c
Khair <khairsmp1@gmail.com>
2024-03-30 22:09:30 +0700

    Adding keranjang relational

--- keranjang/models.py
+++ keranjang/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from user_profile.models import User
+from django.contrib.auth.models import User
+from makanan.models import Makanan
 
 
 # Model Keranjang
 class Keranjang(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    foods = models.ManyToManyField(Makanan)
