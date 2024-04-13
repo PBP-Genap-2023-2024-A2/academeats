a038eb21b80ae02bf558cfd90805ae20a3a53a65
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2024-04-13 20:50:16 +0700

    update feature user profile

--- review/models.py
+++ review/models.py
@@ -5,7 +5,8 @@ from order.models import Order
 
 # Create your models here.
 class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order = models.OneToOneField(Order, on_delete=models.CASCADE)
-    nilai = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True)
+    nilai = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)], default=0)
     komentar = models.TextField()
+    reply = models.TextField(null=True)
--- review/urls.py
+++ review/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from review.views import show_review
+from review.views import show_review, reply_review, create_review
 
 appname = 'review'
 
 urlpatterns = [
-    path('', show_review, name='show_review')
+    path('<int:order_id>/', show_review, name='show_review'),
+    path('<int:order_id>/create/', create_review, name='create_review'),
+    path('reply/<int:id>/', reply_review, name='reply_review')
 ]
