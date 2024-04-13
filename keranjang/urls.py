35062ff18b3f509f9400fe6405cf134757e48092
Khair <khairsmp1@gmail.com>
2024-04-13 17:11:17 +0700

    working on views.py

---
+++ keranjang/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from keranjang.views import show_main
+
+app_name = 'keranjang'
+
+urlpatterns = [
+    path('keranjang/', show_main, name=show_main),
+]
--- pacil_food/urls.py
+++ pacil_food/urls.py
@@ -21,5 +21,10 @@ urlpatterns = [
     path('', include(('main.urls', 'main'), namespace='main')),
     path('u/', include(('user_profile.urls', 'user_profile'), namespace='user-profile')),
     path('toko/', include(('toko.urls', 'toko'), namespace='toko')),
+    path('makanan/', include(('makanan.urls', 'makanan'), namespace='makanan')),
+    path('keranjang/', include(('keranjang.urls', 'keranjang'), namespace='keranjang')),
+    path('order/', include(('order.urls', 'order'), namespace='order')),
+    path('review/', include(('review.urls', 'review'), namespace='review')),
+    path('forum/', include(('forum.urls', 'forum'), namespace='forum')),
     path('admin/', admin.site.urls),
 ]
