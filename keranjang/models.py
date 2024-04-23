391689eebe8edcfed078e9f5f453597abde1325b
Carleano Ravelza Wongso <carleanoravel@gmail.com>
2024-04-23 23:09:09 +0700

    Fixing cart

--- keranjang/models.py
+++ keranjang/models.py
@@ -6,4 +6,4 @@ from makanan.models import Makanan
 class ItemKeranjang(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     makanan = models.ForeignKey(Makanan, on_delete=models.CASCADE)
-    jumlah = models.IntegerField(default=0)
+    jumlah = models.IntegerField(default=1)
