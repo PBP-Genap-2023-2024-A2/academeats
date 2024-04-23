0e7ac9db959100f9f9032d6ee402d7df1f7a3391
kanialmyra <kania.almyra@ui.ac.id>
2024-04-24 02:52:13 +0700

    update order

--- order/views.py
+++ order/views.py
@@ -31,15 +31,9 @@ def show_main_pembeli(request):
     order_list = []
 
     for og in order_group:
-        print(og)
-        order_list.append(orders.filter(order_group = og))
+        o = orders.filter(order_group = og)
 
-    for ods in order_list:
-        total_harga = 0
-        pesanan_cnt = 0
-        for od in ods:
-            total_harga += od.makanan.harga * od.quantity
-            pesanan_cnt += 1
+        order_list.append({'pk': og.pk, 'id': og.orderID, 'orders': o, 'status': aggregat_status(o)})
 
     context = {
         'order_group': order_list,
@@ -67,33 +61,28 @@ def edit_status_penjual(request):
     if request.method == 'POST':
         order_id = request.POST.get('order_id')
         order = Order.objects.get(pk=int(order_id))
+
+        if (order.status == "DIBATALKAN"):
+            return HttpResponse(400)
+
         order_status = request.POST.get('order_status')
         order.status = getattr(Order.StatusPesanan, order_status)
         order.save()
+
         return JsonResponse({'status': 'success'})
     else:
-        pass
         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
-def edit_status_batal(request, order_id):
-    if request.method == 'POST':
-        order = get_object_or_404(Order, orderID=order_id)
+@csrf_exempt
+def edit_status_batal(request, og_id):
+    order_group = OrderGroup.objects.get(pk=og_id)
+    orders = Order.objects.filter(order_group=order_group)
+
+    for order in orders:
         order.status = Order.StatusPesanan.DIBATALKAN
         order.save()
-        return JsonResponse({'status': 'success'})
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
-def update_status_batal(request):
-    if request.method == 'POST':
-        order_group_id = request.POST.get("og_id")
-        order_group = OrderGroup.objects.get(pk=int(order_group_id))
-        orders = Order.objects.filter(order_group=order_group)
-
-        for order in orders:
-            order.status = Order.StatusPesanan.DIBATALKAN
-            order.save()
-
+    return HttpResponse(status=200)
 
 def delete_order(request, order_id):
     order = Order.objects.get(order_id)
@@ -103,3 +92,23 @@ def delete_order(request, order_id):
     order.delete()
 
     return HttpResponse('DELETED')
+
+
+# HELPER FUNCTION
+def aggregat_status(orders):
+    statuses = []
+    status = ""
+
+    for order in orders:
+        statuses.append(order.status)
+
+    for s in statuses:
+        if s == "DIBATALKAN":
+            status = "DIBATALKAN"
+            break
+        elif s == "DIPESAN" or s == "DIPROSES":
+            status = "DIPROSES"
+            break
+    else:
+        status = "SELESAI"
+    return status
