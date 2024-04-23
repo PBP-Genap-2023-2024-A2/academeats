c7ebc284ad2d890f108c67f55d685f84a7958f90
nairisfa <nailakrisnazulfa@gmail.com>
2024-04-23 23:09:33 +0700

    adding features on feat/makanan

--- makanan/urls.py
+++ makanan/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from makanan import views
 
 urlpatterns = [
-    path('<int:makanan_id>/', views.detail_makanan, name="detail")
+    path('<int:makanan_id>/', views.detail_makanan, name="detail"),
+    path('<int:makanan_id>/edit', views.edit_makanan, name='edit')
 ]
--- makanan/views.py
+++ makanan/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from makanan.models import Makanan
 from toko.models import Toko
 from makanan.forms import MakananForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def detail_makanan(request, makanan_id):
@@ -12,3 +14,34 @@ def detail_makanan(request, makanan_id):
 def get_stok(request):
     makanan = Makanan.objects.get(pk=request.GET.get('makanan_id'))
     return render(request, 'get_stok.html', {'makanan': makanan})
+
+def show_makanan(request):
+    makanan = Makanan.objects.all()
+
+    context = {
+        'name': request.user.username,
+        'makanan': makanan
+
+    }
+
+    return render(request, 'main_makanan.html', context)
+
+
+def delete_makanan(request, makanan_id):
+    makanan = Makanan.objects.get(pk=makanan_id)
+    makanan.delete()
+    return HttpResponseRedirect(reverse('main:index'))
+
+def edit_makanan(request, makanan_id):
+    makanan = Makanan.objects.get(pk = makanan_id)
+
+
+    form = MakananForm(request.POST or None, instance=makanan)
+
+    if form.is_valid() and request.method == "POST":
+       
+        form.save()
+        return HttpResponseRedirect(reverse('main:index'))
+
+    context = {'form': form}
+    return render(request, "edit_makanan.html", context)
