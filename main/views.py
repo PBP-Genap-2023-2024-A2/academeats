c08446f2f50faf98acb009e3a53d229fa0369e2a
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2024-04-29 07:41:04 +0700

    fix bug

--- main/views.py
+++ main/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 
 from user_profile.models import Profile
 from utils.decorators import penjual_only, pembeli_only
-from django.contrib.auth.decorators import login_required
 
-@login_required
 def index(request):
     user = request.user
-    if (user.profile.role == 'penjual'):
-        return render(request, 'index_penjual.html', {'user' : user})
-    return render(request, 'index_pembeli.html', {'user' : user})
-
-
+    if user.is_authenticated:
+        if (user.profile.role == 'penjual'):
+            return render(request, 'index_penjual.html', {'user' : user})
+        return render(request, 'index_pembeli.html', {'user' : user})
+    return render(request, 'index.html', {})
+    
 def not_found(request):
     return render(request, '404.html', {})
